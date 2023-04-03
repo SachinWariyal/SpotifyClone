e3459c4b78a50d94c00b49fdad80f95bd8c429e7
Sachin Wariyal <sachinwariyal4@gmail.com>
2023-04-03 17:35:21 +0530

    name changes

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
     {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Mvrda Cartel - Yampi X YOVNGCHIMI", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "They Mad - Lowki Pesci", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Rich the Kid - Plug Walk", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "The Safety Dance - Sleeping at last", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Back it up", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Mockingbird - Eminem", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Love the way you lie - Rihanna", filePath: "songs/2.mp3", coverPath: "covers/10.jpg"},
+    {songName: "The last Dance - heroinica", filePath: "songs/4.mp3", coverPath: "covers/9.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
